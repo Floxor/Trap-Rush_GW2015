7d9ab9f054064ad17ae24e2e68413871f22be135
FloFrancois <f.francois@student.isartdigital.com>
2015-04-03 09:52:52 +0200

    Commit tilemap / Forme definitive Integration graphique final à faire

--- src/states/preloaderState.js
+++ src/states/preloaderState.js
@@ -25,7 +25,7 @@ TR_preload.prototype = {
 }
 
 function loadStart () {
-	this.game.load.tilemap('map', 'assets/test60tilemapN1.json', null, Phaser.Tilemap.TILED_JSON);
+	this.game.load.tilemap('map', 'assets/tilemapN5.json', null, Phaser.Tilemap.TILED_JSON);
     this.game.load.spritesheet('tilesetPlaceholder', 'assets/placeholders/collision.png');
     this.game.load.spritesheet('tilesetPlaceholder2', 'assets/placeholders/tileset60x60.png');
 	this.game.load.image('smoke', 'assets/particuleSmoke.png');
